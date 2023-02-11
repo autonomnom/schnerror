e77891170f389cc5042579f5cfd92e4025158b81
autonomnom <christopf@posteo.de>
2023-02-11 14:52:48 +0100

    adding files

--- .eleventy.js
+++ .eleventy.js
@@ -1,6 +1,7 @@
 module.exports = function(eleventyConfig) {
 
     eleventyConfig.addPassthroughCopy("img");
+    eleventyConfig.addPassthroughCopy("files");
 
     //eleventyConfig.addPassthroughCopy({"img/favicon" : "/" });
 };
